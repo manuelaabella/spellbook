454f24887c9cc0bf0dbfe708081b7dd50e14cdd4
manuelaabella <43829770+manuelaabella@users.noreply.github.com>
2019-01-13 21:33:08 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".button-one").click(function() {
+  $(".lumos").toggle();
+});
+
+$(".button-two").click(function() {
+  $(".alohomora").slideToggle();
+});
+
+$(".button-three").click(function() {
+  $(".obliviate").fadeToggle();
+});
+
